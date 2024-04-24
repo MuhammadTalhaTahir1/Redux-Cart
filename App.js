74ac56814b29281d9396382ea010c19cc7a0789c
Talha <talhatahir1455@gmail.com>
2024-04-24 15:12:27 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Main from "./src/Main";
+import Open from "./src/Open";
+import Navigator from "./src/Navigator";
+import { Provider } from "react-redux";
+import Products from "./src/screens/Products";
+import { Store } from "./src/Redux/Store";
+import Cart from "./src/screens/Cart";
+export default function App() {
+  const Stack = createStackNavigator();
+
+  return (
+    <Provider store={Store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="products"
+            component={Products}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Cart"
+            component={Cart}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
